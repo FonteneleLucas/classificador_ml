eac6a408cbe1f28be5379d6a0e7b8d56170c87fd
Lucas Fontenele <lucasfontenele@MacBook-Pro-de-Lucas.local>
2023-03-23 10:15:52 -0300

    adicionando teste com kmeans

--- app/core/classificador.py
+++ app/core/classificador.py
@@ -2,6 +2,10 @@ from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+
 
 class Classificador:
     def classificar(self, renda_mensal: float, valor_divida: float) -> float:
@@ -10,26 +14,47 @@ class Classificador:
 
         array = data[['RM', 'VD']].values
 
+        pca = PCA(2)
+
+        df = pca.fit_transform(array)
+
         # Definindo novo nó a ser classificado
         novo_no = np.array([[renda_mensal, valor_divida]])
 
-        # Obtendo os índices dos vizinhos mais próximos ao novo nó
-        neigh = NearestNeighbors(n_neighbors=3)
-        neigh.fit(array)
-        nn_indices = neigh.kneighbors(novo_no, return_distance=False)
+        # Transformando o novo nó usando PCA
+        novo_no_transformed = pca.transform(novo_no)
 
-        # Obtendo os valores de NP, VP e VD para os vizinhos mais próximos
-        rm_values = data.loc[nn_indices[0], 'RM']
-        vd_values = data.loc[nn_indices[0], 'VD']
-        np_values = data.loc[nn_indices[0], 'NP']
-        vp_values = data.loc[nn_indices[0], 'VP']
+        kmeans = KMeans(n_clusters=6)
 
-        # Obtendo os valores médios de NP, VP e VD para os vizinhos mais próximos
-        rm_mean = np.mean(rm_values)
-        vd_mean = np.mean(vd_values)
-        np_mean = np.mean(np_values)
-        vp_mean = np.mean(vp_values)
+        # Prevendo as labels dos clusters
+        label = kmeans.fit_predict(df)
+
+        # Obtendo as labels únicas
+        u_labels = np.unique(label)
+
+        # Calculando a distância do novo nó para cada centroide
+        centroides = kmeans.cluster_centers_
+        distancias = cdist(novo_no_transformed, centroides)
+
+        # Obtendo o índice do cluster mais próximo
+        cluster_mais_proximo = np.argmin(distancias)
+
+        # Adicionando as labels do cluster aos dados originais
+        data['cluster'] = label
+
+        # Salvando os dados em um arquivo CSV
+        data.to_csv('negociacoes_clusterizado.csv', index=False)
+
+        # Calculando os valores médios para as colunas Q e V por label de cluster
+        cluster_means = data.groupby('cluster')['RM','VD','NP','VP'].mean()
+        # print(cluster_means)
+
+        # Obtendo os valores médios para o cluster mais próximo do novo nó
+        rm_mean = cluster_means.loc[cluster_mais_proximo, 'RM']
+        np_mean = cluster_means.loc[cluster_mais_proximo, 'NP']
+        vd_mean = cluster_means.loc[cluster_mais_proximo, 'VD']
+        vp_mean = cluster_means.loc[cluster_mais_proximo, 'VP']
 
         # Retornando os valores médios para NP, VP, VD para o novo nó
-        print(f"Plano sugerido: RM = {rm_mean:.2f}, VD = {vd_mean:.2f}, NP = {np_mean:.2f}, VP = {vp_mean:.2f}")
+        print(f"O novo nó deve escolher RM = {rm_mean:.2f}, NP = {np_mean:.2f}, VP = {vp_mean:.2f}, VD = {vd_mean:.2f}")
         return rm_mean, vd_mean, np_mean, vp_mean
